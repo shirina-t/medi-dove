1ede236e1b9a7e084cfbe196d16a1dce46359864
shirina-t <shirina.tillyabaeva@gmail.com>
2025-01-31 19:24:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <Button className="bg-slate-600">Click me</Button>
+    </>
+  );
+}
+
+export default App
