cc66e95e5a30f9352c2a4bf4f3acbff4ab9412eb
shirina-t <shirina.tillyabaeva@gmail.com>
2025-01-31 20:48:44 +0500

    installations: i18, lucide, router dom, class var authority

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
+import "./App.css";
+import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const lng = navigator.language;
+    i18n.changeLanguage(lng);
+  }, [])
+  
+  const lng = navigator.language;
 
   return (
     <>
+      <p>{t('greeting.hello')}</p>
+      <p>
+        Browser language: {lng}
+      </p>
       <Button className="bg-slate-600">Click me</Button>
     </>
   );
 }
 
-export default App
+export default App;
