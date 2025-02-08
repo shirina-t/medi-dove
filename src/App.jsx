590a6748d6d006c03730ee7da0b7ee06314ba4f6
shirina-t <shirina.tillyabaeva@gmail.com>
2025-02-08 18:48:26 +0500

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 // import { useTranslation } from "react-i18next";
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home"
 import Layout from "./components/layout";
 import Surgery from './pages/surgery'
 import Details from "./pages/details";
-import Error from "./pages/error";
-import { Home } from "./pages/home"
-import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import About from "./pages/About";
 import Approach from "./pages/Approach";
 import FeedbackAbout from "./pages/FeedbackAbout";
 import TeamAbout from "./pages/TeamAbout";
 import Appointment from "./pages/Appointment";
+import Contact from "./pages/contact";
+import Error from "./pages/error";
 
 function App() {
   return (
@@ -20,15 +20,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/services" element={<Details />} />
           <Route path="/surgery" element={<Surgery />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Details />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/about" element={<About />} />
           <Route path="/approach" element={<Approach />} />
           <Route path="/feedback" element={<FeedbackAbout />} />
           <Route path="/team" element={<TeamAbout />} />
           <Route path="/appointment" element={<Appointment />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
           <Route path="*" element={<Error />} />
       </Routes>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -26,6 +26,7 @@ const Contact = () => {
           </Link>
         </div>
       </div>
+      
     </>
   );
 }
